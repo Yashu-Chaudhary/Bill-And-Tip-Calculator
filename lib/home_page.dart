0a1737839f35dd7d42ba4a46a2efc938f2b5b474
Yashu Chaudhary <yashusky232@gmail.com>
2024-07-28 21:19:13 +0530

    completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,7 +10,14 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   TextEditingController controller = TextEditingController();
-  double price = 0.0;
+  double? price = 0.0;
+  double tipPercentage = 0;
+  int n = 1;
+
+  double totalBill() {
+    double res = (price! + (price! * tipPercentage / 100)) / n;
+    return res;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,22 +45,24 @@ class _HomePageState extends State<HomePage> {
                     style: Style.hedingText2,
                   ),
                   Text(
-                    "$price Rs",
+                    "${totalBill().toStringAsFixed(2)} Rs",
                     style: Style.hedingText3,
                   ),
                 ],
               ),
             ),
           ),
-          const SizedBox(height: 10),
+          const SizedBox(height: 15),
 
           // 3.. textinputfield
           TextField(
             onChanged: (value) {
               setState(() {
-                price = double.parse(controller.text);
+                // logic to check the given text is double or string
+                double.tryParse(controller.text) != null
+                    ? price = double.parse(controller.text)
+                    : price = 0;
               });
-              print(controller.text);
             },
             controller: controller,
             keyboardType: TextInputType.number,
@@ -61,6 +70,72 @@ class _HomePageState extends State<HomePage> {
                 border: OutlineInputBorder(),
                 hintText: 'Please Enter Bill',
                 labelText: 'Please Enter Bill'),
+          ),
+          const SizedBox(height: 15),
+
+          // 3.. split by
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Split By',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+              ),
+              Row(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        setState(() {
+                          if (n > 1) {
+                            n--;
+                          }
+                        });
+                      },
+                      icon: const Icon(Icons.exposure_minus_1)),
+                  Text('$n',
+                      style: const TextStyle(
+                          fontSize: 25, fontWeight: FontWeight.w600)),
+                  IconButton(
+                      onPressed: () {
+                        setState(() {
+                          n++;
+                        });
+                      },
+                      icon: const Icon(Icons.exposure_plus_1)),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 15),
+          const Divider(color: Colors.black, thickness: 2),
+          const SizedBox(height: 15),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Tip Percentage',
+                style: TextStyle(fontSize: 22),
+              ),
+              Text(
+                '${double.parse(tipPercentage.toStringAsFixed(2))}',
+                style:
+                    const TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+
+          Slider(
+            divisions: 10,
+            value: tipPercentage,
+            onChanged: (value) {
+              setState(() {
+                tipPercentage = value;
+              });
+            },
+            min: 0,
+            max: 100,
           ),
         ],
       ),
